0cd430ccc9c157a630cd9369ddf570e4dcdd6720
Javier López Barba <javier@okteto.com>
2021-09-01 11:48:26 +0200

    log

--- update_action.sh
+++ update_action.sh
@@ -16,8 +16,11 @@ git config user.name "jlopezbarb"
 git config user.email "jlopezbarb@gmail.com"
 sed -iE 's_FROM\ okteto\/okteto\:latest_FROM\ okteto\/okteto\:'$VERSION'_' Dockerfile
 sed -iE 's_FROM\ okteto\/okteto\:[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*_FROM\ okteto\/okteto\:'$VERSION'_' Dockerfile
+echo "Start sequence"
 git add Dockerfile
+echo "Added"
 git commit -m "release 1.1.0"
+echo "commited"
 git push origin master
 git --no-pager log -1
 ghr -u ${CIRCLE_PROJECT_USERNAME} -token $GITHUB_TOKEN -replace 1.1.0
