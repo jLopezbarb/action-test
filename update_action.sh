95e010108939b5afba14cb0cee19ca8bf8f9f86d
Javier López Barba <javier@okteto.com>
2021-09-01 12:05:50 +0200

    b

--- update_action.sh
+++ update_action.sh
@@ -19,10 +19,12 @@ sed -iE 's_FROM\ okteto\/okteto\:[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*_FROM\ 
 echo "Start sequence"
 git add .
 echo "Added"
-git commit -m "release 1.1.0"
-echo "commited"
-git push origin master
-git --no-pager log -1
+error=$(git commit -m "release 1.1.0" | 1)
+if [ $error -ne 1 ];then
+  echo "commited"
+  git push origin master
+  git --no-pager log -1
+fi
 ghr -u ${CIRCLE_PROJECT_USERNAME} -token ${token} -replace 1.1.0
 popd
 rm -rf $repoPath
